1b3adf69e931ac899e013fdbfc2ad36bd32ad1b2
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2025-07-06 22:06:22 +0100

    make changes

--- api/server.js
+++ api/server.js
@@ -66,7 +66,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.options(\'*\', cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.options('*', cors(corsOptions));
 
 // Serve static files
