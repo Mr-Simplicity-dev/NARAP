bfaa9d5cd7ff49b8a3fa5fdcacbd61f4b431f44e
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2025-08-14 01:12:57 -0700

    make changes

--- js/admin.js
+++ js/admin.js
@@ -9526,7 +9526,7 @@ function updateCertificatesSelectionUI() {
               maintainAspectRatio: false,
               scales: {
                 x: { beginAtZero: true },
-                y: { ticks: { autoSkip: false, font: { size: 2 } } }
+                y: { ticks: { autoSkip: false, font: { size: 11 } } }
               },
               plugins: {
                 legend: { display: false },
