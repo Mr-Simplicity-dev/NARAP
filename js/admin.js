e9a079ba8718c30437704d34e16d21b54a1e4563
Abdulkareem Onimisi Moshood <moshood2uall@gmail.com>
2025-08-17 02:42:48 -0700

    make changes

--- js/admin.js
+++ js/admin.js
@@ -30,7 +30,8 @@ class PerformanceMonitor {
     }
 }
 
-
+// Notification Manager Class
+// Notification Manager Class (ghost box safe)
 // Notification Manager Class (fixed: no ghost box, no duplicates)
 class NotificationManager {
   constructor() {
@@ -11903,5 +11904,3 @@ try {
 
   console.log('✅ State Select Patch (merged) active');
 })();
-
-
